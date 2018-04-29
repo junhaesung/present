750d8cd593865b4f0151385f54bc012591184071
Haeseong Jeon <junhaesung0428@hanmail.net>
2018-04-29 12:20:01 +0900

    Add Swagger

--- src/main/java/com/depromeet/present/PresentApplication.java
+++ src/main/java/com/depromeet/present/PresentApplication.java
@@ -2,8 +2,10 @@ package com.depromeet.present;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class PresentApplication {
 
 	public static void main(String[] args) {
