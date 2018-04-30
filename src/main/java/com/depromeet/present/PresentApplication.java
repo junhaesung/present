6031da9a67f204a29114e78274a49aae7eb14d27
Haeseong Jeon <junhaesung0428@hanmail.net>
2018-05-01 01:34:54 +0900

    Add JPA configuration

--- src/main/java/com/depromeet/present/PresentApplication.java
+++ src/main/java/com/depromeet/present/PresentApplication.java
@@ -2,10 +2,12 @@ package com.depromeet.present;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableJpaRepositories(basePackages = "com.depromeet.present.repository")
 public class PresentApplication {
 
 	public static void main(String[] args) {
